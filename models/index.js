a7fe9476812a02ea9ede6358f8f61d98f437d611
Ryan Kim <82355520+miknayr@users.noreply.github.com>
2021-07-12 15:25:31 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -26,7 +26,7 @@ const connect = () => {
 module.exports = {
   connect,
   User: mongoose.model('user', require('./User')),
-  Friend: mongoose.model('friend', require('./Friend'))
+  Friend: mongoose.model('friend', require('./Friend')),
   Event: mongoose.model('event', require('./Event'))
 
 }
