0b87e62e5f6caf0dc1562493640b7050f1bd6ee8
miknayr <teamry4n@gmail.com>
2021-07-14 14:04:21 -0700

    user and events commented out

--- models/User.js
+++ models/User.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
-  name: String, 
+  name: {
+    type:String, 
+    required:true
+  },
   password: String,
-  email: String,
-  friends:[{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:'user'
-}],
+  email: {
+    type:String,
+    required:true
+  },
+  friends: [{
+    type: mongoose.Schema.Types.ObjectId, ref:'User'
+  }],
+  // event: [{
+  //   type: mongoose.Schema.Types.ObjectId, ref:'User'
+  // }],
 }, {
   timestamps: true
 })
