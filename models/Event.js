0c6eae29927d78fa5c0afcd17ff12eaac7af1fb7
miknayr <teamry4n@gmail.com>
2021-07-14 18:28:14 -0700

    cleaned up stuff

--- models/Event.js
+++ models/Event.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose')
 
 const EventSchema = new mongoose.Schema({
   eventName: String,
-  location: Array,
-  // friend: mongoose.Schema.Types.ObjectId, ref:'User'
   
+  location: [{
+    type: mongoose.Schema.Types.ObjectId, ref:'Location'
+  }]
   
 }, {
   timestamps: true
