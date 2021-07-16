0a33e9639534d660434060aee3bcfb0785749b3a
miknayr <teamry4n@gmail.com>
2021-07-16 10:24:24 -0700

    updated again

--- models/Location.js
+++ models/Location.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const LocationSchema = new mongoose.Schema({
 
-    name: String,
+    name: String
+    ,
 
     user: [{
         type: mongoose.Schema.Types.ObjectId, ref:'User'
